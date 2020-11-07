49604f36605c9d33c5b618cc024e18808fd7acc8
Mando Estrada <libryum33@hotmail.com>
2020-11-07 09:46:00 -0800

    adding comments

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// set up for heroku and mongodb
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
